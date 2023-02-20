f258ef24f1b9ae3884ce647d44d5a80c48d3b99f
Luiz Felipe Bonatto <luizfelipebonatto@hotmail.com>
2023-02-20 13:39:01 -0300

    Ready to deploy v1

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 
 const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
@@ -32,5 +33,5 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
